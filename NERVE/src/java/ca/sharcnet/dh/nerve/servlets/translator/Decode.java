c281f9c82f9bb49c080b07a99bae323b2658dbdb
edwar <edwar@192.168.0.162>
2017-04-19 08:48:01 -0400

    1.5.1 alpha Renamed a few files.

--- NERVE/src/java/ca/sharcnet/dh/nerve/servlets/translator/Decode.java
+++ NERVE/src/java/ca/sharcnet/dh/nerve/servlets/translator/Decode.java
@@ -2,21 +2,12 @@ package ca.sharcnet.dh.nerve.servlets.translator;
 import ca.sharcnet.dh.nerve.servlets.CustomServlet;
 import ca.sharcnet.nerve.decode.Decoder;
 import ca.fa.utility.streams.StreamUtil;
-import ca.sharcnet.nerve.Constants;
-import ca.sharcnet.nerve.context.Context;
-import ca.sharcnet.nerve.context.ContextLoader;
+import ca.sharcnet.nerve.HasStreams;
 import ca.sharcnet.nerve.docnav.DocumentLoader;
-import ca.sharcnet.nerve.docnav.dom.Attribute;
-import ca.sharcnet.nerve.docnav.dom.AttributeNode;
 import ca.sharcnet.nerve.docnav.dom.Document;
-import ca.sharcnet.nerve.docnav.dom.Node;
-import ca.sharcnet.nerve.docnav.dom.NodeType;
-import ca.sharcnet.nerve.docnav.selector.Select;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.Properties;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,28 +16,15 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.json.JSONObject;
 
 @WebServlet(name = "Decode", urlPatterns = {"/Decode.do"})
-public class Decode extends CustomServlet {
+public class Decode extends CustomServlet implements HasStreams {
 
     @Override
     protected boolean processRequest(HttpServletRequest request, HttpServletResponse response, JSONObject json) throws ServletException, IOException {
         try {
             JSONObject inJSON = StreamUtil.getJSON(request.getInputStream());
-            String inputString = inJSON.getString("input");
-            InputStream cfgStream = this.getServletContext().getResourceAsStream("/WEB-INF/config.txt");
-            Properties config = new Properties();
-            config.load(cfgStream);
-
-            Document document = DocumentLoader.documentFromString(inputString);
-            System.out.println(document);
-
-            /* check document for schema to set the context */
-            Select selected = document.select().attribute(Constants.ORIGINAL_TAGNAME_ATTR, "xml-model");
-            if (selected.isEmpty()) throw new RuntimeException("Schema instruction element not found.");
-            String contextName = selected.get(0).getAttributeValue(Constants.CONTEXT_ATTRIBUTE);
-            contextName = "/res/" + contextName.toLowerCase() + ".context.json";
-            Context context = ContextLoader.load(Encode.class.getResourceAsStream(contextName));
-            Document decoded = new Decoder().decode(document, context);
-
+            String asString = "<doc>\n" + inJSON.getString("input") + "\n</doc>";
+            Document document = DocumentLoader.documentFromString(asString);
+            Document decoded = Decoder.decode(document, this);
             json.put("result", "processed");
             json.put("output", decoded);
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException | ParserConfigurationException ex) {
@@ -55,5 +33,10 @@ public class Decode extends CustomServlet {
         }
 
         return true;
+    }
+
+    @Override
+    public InputStream getResourceStream(String path) {
+        return Decode.class.getResourceAsStream("/res/" + path);
     }
 }
--- NERVE/src/java/ca/sharcnet/dh/nerve/servlets/translator/Encode.java
+++ NERVE/src/java/ca/sharcnet/dh/nerve/servlets/translator/Encode.java
@@ -1,28 +1,18 @@
 package ca.sharcnet.dh.nerve.servlets.translator;
-import ca.fa.utility.sql.SQL;
+
 import ca.sharcnet.dh.nerve.servlets.CustomServlet;
 import ca.sharcnet.nerve.docnav.DocumentLoader;
 import ca.sharcnet.nerve.docnav.antlr.InvalidTokenException;
 import ca.sharcnet.nerve.docnav.dom.Document;
-import ca.sharcnet.nerve.encoder.Classifier;
 import ca.sharcnet.nerve.encoder.ClassifierException;
 import ca.sharcnet.nerve.encoder.Encoder;
 import ca.fa.utility.streams.StreamUtil;
-import ca.sharcnet.nerve.context.*;
-import ca.sharcnet.nerve.docnav.dom.Attribute;
-import ca.sharcnet.nerve.docnav.dom.InstructionNode;
-import ca.sharcnet.nerve.docnav.dom.NodeType;
-import ca.sharcnet.nerve.docnav.schema.Schema;
-import ca.sharcnet.nerve.docnav.schema.relaxng.RelaxNGSchemaLoader;
-import java.io.BufferedInputStream;
+import ca.sharcnet.nerve.HasStreams;
 import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.sql.SQLException;
-import java.util.Properties;
-import java.util.zip.GZIPInputStream;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,7 +21,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.json.JSONObject;
 
 @WebServlet(name = "Encode", urlPatterns = {"/Encode.do"})
-public class Encode extends CustomServlet {
+public class Encode extends CustomServlet implements HasStreams {
 
     @Override
     protected boolean processRequest(HttpServletRequest request, HttpServletResponse response, JSONObject json) throws ServletException, IOException, FileNotFoundException {
@@ -39,52 +29,8 @@ public class Encode extends CustomServlet {
             JSONObject inJSON = StreamUtil.getJSON(request.getInputStream());
             String inputString = inJSON.getString("input");
             ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
-            InputStream cfgStream = this.getServletContext().getResourceAsStream("/WEB-INF/config.txt");
-            Properties config = new Properties();
-            config.load(cfgStream);
-            SQL sql = new SQL(config);
-
-            Encoder encoder;
-            InputStream resourceAsStream = super.getServletContext().getResourceAsStream(config.getProperty("classifier"));
-            assert resourceAsStream != null;
-            BufferedInputStream bis = new BufferedInputStream(new GZIPInputStream(resourceAsStream));
-            Document doc = DocumentLoader.documentFromStream(inputStream);
-
-            /* check document for schema to set the context */
-            System.out.println(doc);
-            InstructionNode iNode = doc.getInstructionNode("xml-model");
-
-            Context context = null;
-            if (iNode.getType() == NodeType.INSTRUCTION){
-                if (iNode.hasAttribute("href")){
-                    Attribute attr = iNode.getAttribute("href");
-                    String value = attr.getValue();
-
-                    if (value.contains("orlando_biography_v2.rng")){
-                        context = ContextLoader.load(Encode.class.getResourceAsStream("/res/orlando.context.json"));
-                    }
-                    else if (value.contains("cwrc_entry.rng")){
-                        context = ContextLoader.load(Encode.class.getResourceAsStream("/res/cwrc.context.json"));
-                    }
-                    else if (value.contains("cwrc_tei_lite.rng")){
-                        context = ContextLoader.load(Encode.class.getResourceAsStream("/res/tei.context.json"));
-                    }
-                }
-            }
-
-            encoder = new Encoder(doc, context, sql, new Classifier(bis));
-            resourceAsStream.close();
-
-            /** add the schema **/
-            String schemaURL = context.schemaName;
-            if (schemaURL != null && !schemaURL.isEmpty()) {
-                InputStream schemaStream = new URL(schemaURL).openStream();
-                Schema schema = RelaxNGSchemaLoader.schemaFromStream(schemaStream);
-                encoder.setSchema(schema);
-            }
-
-            Document encoded = encoder.encode();
-
+            Document document = DocumentLoader.documentFromStream(inputStream);
+            Document encoded = Encoder.encode(document, this);
             json.put("result", "processed");
             json.put("output", encoded);
         } catch (InvalidTokenException | ParserConfigurationException | ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException | ClassifierException | ClassCastException | NullPointerException ex) {
@@ -92,5 +38,10 @@ public class Encode extends CustomServlet {
             return false;
         }
         return true;
+    }
+
+    @Override
+    public InputStream getResourceStream(String path) {
+        return Encode.class.getResourceAsStream("/res/" + path);
     }
 }
