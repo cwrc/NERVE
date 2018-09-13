53b105f8a76b0e179d8606b8f60c2b6fa8cddf8f
edwar <edwar@192.168.0.162>
2018-09-13 12:20:43 -0400

    added nidget context files

---
+++ NidgetContext/src/js/NidgetContext.js
@@ -0,0 +1,222 @@
+const Widget = require("nidget/src/Widget");
+
+class NidgetMenuItem extends Widget {
+    constructor(nidgetContextMenu, labelText, options, handler) {
+        super(`<div class="nidget-context-menuitem"></div>`);
+        this.nidgetContextMenu = nidgetContextMenu;
+        this.label = $(` <div class="nidget-context-label">${labelText}</div>`);
+        this.image = $(`<img class="nidget-context-left-image" src=""/>`);
+        this.throbber = $(`<img class="nidget-context-right-image nidget-context-image-throbber" src="assets/nidget_context/loader400.gif"/>`);       
+        
+        this.$.append(this.image);
+        this.$.append(this.label);
+        this.$.append(this.throbber);
+
+        this.image.hide();
+
+        this.options = {
+            async : false,
+            close_menu : true
+        };
+        
+        if (typeof options === "function") {
+            this.handler = options;
+        } else if (typeof options === "object") {
+            for (let field in options) this.options[field] = options[field];            
+            if (typeof handler === "function") this.handler = handler;
+        }
+
+        if (this.handler){
+            if (this.options.async){
+                this.__setupAsyncClick();
+            }
+            else if (!this.options.async){
+                this.__setupSyncClick();    
+            }
+        }
+        else{
+            this.__setupNoHandler();
+        }
+    }
+
+    setImage(imagePath){
+        this.image.attr("src", imagePath);
+        this.image.show();
+    }
+
+    __setupNoHandler(){
+        this.$.click((event) => {
+            if (this.options.close_menu) this.nidgetContextMenu.hide();
+            event.stopPropagation();
+        });
+    }
+
+    __setupAsyncClick() {
+        this.$.click(async (event) => {
+            this.throbber.show();
+            await this.handler();
+            this.throbber.hide();
+            if (this.options.close_menu) this.nidgetContextMenu.hide();
+            event.stopPropagation();
+        });
+    }
+
+    __setupSyncClick() {
+        this.$.click((event) => {
+            this.handler();
+            if (this.options.close_menu) this.nidgetContextMenu.hide();
+            event.stopPropagation();
+        });
+    }
+
+    enable() {
+        this.label.removeClass("disabled");
+        return this;
+    }
+
+    disable() {
+        this.label.addClass("disabled");
+        return this;
+    }
+}
+
+class NidgetContextBase extends Widget {
+
+    constructor(element){
+        super(element);
+        this.menuItems = new Map();        
+    }
+
+    getMenuItem(displayString){
+        return this.menuItems.get(displayString);
+    }
+
+    notifyContextChange(context) {
+        this.context = context;
+    }
+
+    setDictionary(dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    show(event){
+        this.$.show();
+        this.position(this.$.get(0), event);
+        this.notifyListeners("notifyContextShow", this);
+    }
+    
+    hide(){
+        this.$.hide();
+    }
+
+    position(element, event) {
+        let posX = event.clientX - 5;
+        let posY = event.clientY - 5;
+        element.style.left = posX + "px";
+        element.style.top = posY + "px";
+    }
+}
+
+class NidgetContext extends NidgetContextBase{
+    constructor() {
+        super(`<div class="nidget-context-dialog nidget-context-menu"></div>`);
+        $("body").append(this.$);
+        
+        this.$.on("contextmenu", ()=>{
+            this.hide();
+            return false;
+        });
+
+        this.hide();
+    }
+    
+    addMenuItem(displayString, options, handler) {
+        let menuItem = new NidgetMenuItem(this, displayString, options, handler);
+        this.$.append(menuItem.$);
+        this.menuItems.set(menuItem);
+        return menuItem;
+    }
+
+    addSubMenu(displayString, options) {
+        let menuItem = new NidgetSubMenu(this, displayString, options);
+        this.$.append(menuItem.$);
+        this.menuItems.set(menuItem);
+        return menuItem;
+    }    
+}
+
+class NidgetSubMenu extends NidgetContextBase{
+    constructor(nidgetContextMenu, labelText) {
+        super(`<div class="nidget-context-menuitem"></div>`);
+
+        this.image = $(`<img class="nidget-context-left-image" src=""/>`);
+        this.label = $(` <div class="nidget-context-label">${labelText}</div>`);        
+        this.arrow = $(`<img class="nidget-context-right-arrow" src="assets/nidget_context/submenu_arrow.png"/>`);               
+        this.container = $(`<div class="nidget-context-submenu nidget-context-menu"><div>`);
+    
+        this.nidgetContextMenu = nidgetContextMenu;
+    
+        this.orgLeft = this.container.css("left");
+    
+        this.$.append(this.image);
+        this.$.append(this.label);
+        this.$.append(this.arrow);
+        this.$.append(this.container);
+        
+        this.container.hide();
+        
+        this.$.on("mouseenter", ()=>{
+            this.showContainer();
+        });
+        
+        this.$.on("mouseleave", ()=>{
+            this.hideContainer();
+        });        
+    }
+    
+    addMenuItem(displayString, options, handler) {
+        let menuItem = new NidgetMenuItem(this, displayString, options, handler);
+        this.container.append(menuItem.$);
+        this.menuItems.set(menuItem);
+        return menuItem;
+    }
+
+    addSubMenu(displayString, options) {
+        let menuItem = new NidgetSubMenu(this, displayString, options);
+        this.container.append(menuItem.$);
+        this.menuItems.set(menuItem);
+        return menuItem;
+    }    
+    
+    showContainer(){
+        this.showContainerTO = window.setTimeout(()=>{
+            this.container.show();
+            this.__positionContainer();
+            this.showContainerTO = null;
+        }, 200);
+    }
+    
+    __positionContainer(){
+        if (!this.orgLeft) this.orgLeft = this.container.css("left");
+        
+        this.container.get(0).style.left = this.orgLeft;
+        let right = this.container.offset().left + this.container.width();
+        let diff = $("body").innerWidth() - right;        
+        
+        if (diff < 0){
+            let pos = `calc(${this.orgLeft} + ${diff}px)`;
+            this.container.get(0).style.left = pos;
+        }         
+    }
+    
+    hideContainer(){
+        if (this.showContainerTO !== null){
+            window.clearTimeout(this.showContainerTO);
+            this.showContainerTO = null;
+        } else {
+            this.container.hide();
+        }
+    }    
+}
+
+module.exports = NidgetContext;
