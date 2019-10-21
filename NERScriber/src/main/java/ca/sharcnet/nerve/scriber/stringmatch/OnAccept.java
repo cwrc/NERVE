2a38fed91c1b55a7ec5882f7b76c8867480aa4d0
Ed Armstrong <ed@frar.ca>
2019-10-21 10:19:17 -0400

    removed string match

--- NERScriber/src/main/java/ca/sharcnet/nerve/scriber/stringmatch/OnAccept.java
+++
@@ -1,5 +0,0 @@
-package ca.sharcnet.nerve.scriber.stringmatch;
-
-public interface OnAccept <NODE>{
-    public void accept(String string, NODE value);
-}
--- NERScriber/src/main/java/ca/sharcnet/nerve/scriber/stringmatch/OnReject.java
+++
@@ -1,5 +0,0 @@
-package ca.sharcnet.nerve.scriber.stringmatch;
-
-public interface OnReject {
-    public void reject(String string);
-}
